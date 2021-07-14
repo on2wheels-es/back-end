7de12786b75afaab9b8673e24719befcf3f9da10
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-14 15:37:12 +0200

    adds mountainPass model and updates route and location ones

---
+++ models/Mountain_Pass.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+puerto	provincia	pueblo	altitud	desnivel	distancia	pendiente	coeficiente	url	coords
+
+const mountainPassSchema = new Schema(
+	{
+		_id: { type: Schema.Types.ObjectId },
+		name: String,
+        province: String,
+		municipality: String,
+		altitude: Number,
+		gradient: Number,
+		distance: Number,
+        mountain_slope: Number,
+        coefficient: Number,
+        url: String,
+		photo: String,
+		geolocation: {
+			type: {
+				type: String,
+				default: 'Point',
+			},
+			coordinates: Array,
+		},
+        routes_ids: [{ type: Schema.Types.ObjectId, ref: 'Routes' }],
+		locations_ids: [{ type: Schema.Types.ObjectId, ref: 'Location' }],
+	},
+	{ timestamps: true }
+);
+
+const MountainPass = mongoose.model('MountainPass', mountainPassSchema);
+
+module.exports = MountainPass;
