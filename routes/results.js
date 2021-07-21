779965d0fcf088cd1f7877924199513392813640
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-21 16:28:11 +0200

    limits GET searchResults resquest to 8 for testing

--- routes/results.js
+++ routes/results.js
@@ -4,10 +4,8 @@ const router = express.Router();
 const Municipality = require('../models/Municipality');
 
 router.get('/search', async (req, res, next) => {
-	console.log(req.query)
-
 	try {
-		const municipality = await Municipality.find().limit(2);
+		const municipality = await Municipality.find().limit(8);
 		res.json(municipality);
 	} catch (e) {
 		next(e);
