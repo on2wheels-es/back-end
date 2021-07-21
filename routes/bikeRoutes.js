0ca402270d5d5a4e378afd972a0d130a0e38b7aa
Laia Lloret <laialloret95@gmail.com>
2021-07-21 15:17:52 +0200

    improves findById routes to get all the detail information

--- routes/bikeRoutes.js
+++ routes/bikeRoutes.js
@@ -18,7 +18,7 @@ router.get('/:id', async (req, res, next) => {
 
 	try {
 		const route = await Route.findById(id).populate('mountain_passes_ids');
-		const municipalities = await Municipality.find({ routes_ids: id });
+		const municipalities = await Municipality.find({ routes_ids: { $in: [id] } });
 		res.json({ route, municipalities });
 	} catch (e) {
 		next(e);
