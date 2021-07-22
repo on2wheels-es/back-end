0a97f8be7559855c49bfaecc9860b5ece194d00e
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-22 18:25:48 +0200

    request query strings in results route

--- routes/results.js
+++ routes/results.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const Municipality = require('../models/Municipality');
+const url = require('url');
+const querystring = require('querystring')
 
 router.get('/search', async (req, res, next) => {
 	try {
-		const municipality = await Municipality.find().limit(6);
+		const { location } = req.query;
+		const municipality = await Municipality.find({ ccaa: location }).limit(6);
 		res.json(municipality);
 	} catch (e) {
 		next(e);
