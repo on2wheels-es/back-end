4c4bee867817f8eb54e23b7ae42a26d7c16c93b3
Laia Lloret <laialloret95@gmail.com>
2021-07-22 18:45:55 +0200

    fix

--- routes/results.js
+++ routes/results.js
@@ -2,13 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const Municipality = require('../models/Municipality');
-const url = require('url');
-const querystring = require('querystring')
 
 router.get('/search', async (req, res, next) => {
 	try {
-		const { location } = req.query;
-		const municipality = await Municipality.find({ ccaa: location }).limit(6);
+		console.log(req);
+		const municipality = await Municipality.find({ ccaa: req.query.location }).limit(6);
 		res.json(municipality);
 	} catch (e) {
 		next(e);
