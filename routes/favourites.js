5ff4a5b5b0dd2a85f30db1bf091a254b6bfd6c5f
Laia Lloret <laialloret95@gmail.com>
2021-07-24 12:47:57 +0200

    create all fav routes

--- routes/favourites.js
+++ routes/favourites.js
@@ -5,22 +5,65 @@ const User = require('../models/User');
 
 router.patch('/addToFavourites/:id/:type', async (req, res, next) => {
 	const { id, type } = req.params;
-	// eslint-disable-next-line no-underscore-dangle
-	const userID = req.session.currentUser._id;
+	const { userID } = req.body;
 
 	try {
 		if (type === 'mountainPasses') {
 			const user = await User.findByIdAndUpdate(userID, { $push: { favouritePasses: id } }, { new: true });
-			req.session.currentUser = user;
 			return res.json(user);
 		}
 		if (type === 'routes') {
 			const user = await User.findByIdAndUpdate(userID, { $push: { favouriteRoutes: id } }, { new: true });
-			req.session.currentUser = user;
 			return res.json(user);
 		}
 		const user = await User.findByIdAndUpdate(userID, { $push: { favouriteLocations: id } }, { new: true });
-		req.session.currentUser = user;
+		return res.json(user);
+	} catch (error) {
+		return next(error);
+	}
+});
+
+router.post('/checkIfFav', async (req, res, next) => {
+	const { id, type, userID } = req.body;
+	try {
+		if (type === 'mountainPasses') {
+			const user = await User.findById(userID);
+			if (user.favouritePasses.includes(id)) {
+				return res.json({ isFavourited: true });
+			}
+			return res.json({ isFavourited: false });
+		}
+		if (type === 'routes') {
+			const user = await User.findById(userID);
+			if (user.favouriteRoutes.includes(id)) {
+				return res.json({ isFavourited: true });
+			}
+			return res.json({ isFavourited: false });
+		}
+		const user = await User.findById(userID);
+		if (user.favouriteLocations.includes(id)) {
+			return res.json({ isFavourited: true });
+		}
+		return res.json({ isFavourited: false });
+	} catch (error) {
+		return next(error);
+	}
+});
+
+router.patch('/removeFromFavourites/:id/:type', async (req, res, next) => {
+	const { id, type } = req.params;
+	const { userID } = req.body;
+
+	try {
+		if (type === 'mountainPasses') {
+			const user = await User.findByIdAndUpdate(userID, { $pull: { favouritePasses: id } }, { new: true });
+			return res.json(user);
+		}
+		if (type === 'routes') {
+			const user = await User.findByIdAndUpdate(userID, { $pull: { favouriteRoutes: id } }, { new: true });
+			return res.json(user);
+		}
+		const user = await User.findByIdAndUpdate(userID, { $pull: { favouriteLocations: id } }, { new: true });
 		return res.json(user);
 	} catch (error) {
 		return next(error);
