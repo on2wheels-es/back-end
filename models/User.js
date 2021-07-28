0e68bfdebcbf0ae1cad6fb0c36f69410256cd473
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-28 18:43:00 +0200

    adds new field in user model 'profileComplete'

--- models/User.js
+++ models/User.js
@@ -33,6 +33,7 @@ const userSchema = new Schema(
 		favouriteRoutes: [{ type: Number, ref: 'Routes' }],
 		favouritePasses: [{ type: Number, ref: 'MountainPasses' }],
 		favouriteLocations: [{ type: Number, ref: 'Locations' }],
+		profileComplete: Boolean,
 	},
 	{ timestamps: true }
 );
