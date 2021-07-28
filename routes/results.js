73e902aecb0569e6a4454b0099e6dc64299e5dd5
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-28 15:22:08 +0200

    calls the weather api from the results route and install axios

--- routes/results.js
+++ routes/results.js
@@ -1,13 +1,20 @@
 const express = require('express');
-
+const axios = require("axios");
 const router = express.Router();
 
+// eslint-disable-next-line no-unused-vars
 const Municipality = require('../models/Municipality');
 
-router.get('/search', async (req, res, next) => {
+router.post('/search', async (req, res, next) => {
+	console.log(req.body)
+	const { middleDateForApiRequest, locations } = req.body;
+
 	try {
-		const municipality = await Municipality.find({ ccaa: req.query.location }).limit(6);
-		res.json(municipality);
+		axios
+			.get(`http://www.on2wheels.es/api/weather?${middleDateForApiRequest}&ccaa=${locations}`)
+			.then(response => console.log(response));
+		// const municipality = await Municipality.find({ ccaa: req.query.location }).limit(6);
+		// res.json(municipality);
 	} catch (e) {
 		next(e);
 	}
