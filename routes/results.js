122ce57c4a8f5e269fb09ea48caa2b92859bb974
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-28 17:17:38 +0200

    adds  weather api request

--- routes/results.js
+++ routes/results.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const axios = require("axios");
+const axios = require('axios');
+
 const router = express.Router();
 
-// eslint-disable-next-line no-unused-vars
 const Municipality = require('../models/Municipality');
 
-router.post('/search', async (req, res, next) => {
-	console.log(req.body)
+router.post('/search', (req, res, next) => {
+	console.log(req.body);
 	const { middleDateForApiRequest, locations } = req.body;
 
-	try {
-		axios
-			.get(`http://www.on2wheels.es/api/weather?${middleDateForApiRequest}&ccaa=${locations}`)
-			.then(response => console.log(response));
-		// const municipality = await Municipality.find({ ccaa: req.query.location }).limit(6);
-		// res.json(municipality);
-	} catch (e) {
-		next(e);
-	}
+	axios.get(`http://www.on2wheels.es/api/weather?${middleDateForApiRequest}&ccaa=${locations}`).then(response => {
+		const { destination } = response.data;
+
+		Municipality.find({ _id: { $in: destination } })
+			.then(municipalities => {
+				res.json(municipalities);
+			})
+			.catch(error => next(error));
+	});
 });
 
 module.exports = router;
